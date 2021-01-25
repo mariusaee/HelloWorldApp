30cc461253ffa1bb678fdc51412042f8982a7f57
mariusaee <mariusae@me.com>
2021-01-25 20:45:32 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Marius Malyshev on 25.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
